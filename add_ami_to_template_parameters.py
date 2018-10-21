40745a7de655dec44e9d27443723b9320d8db8e4
Alex Kinnane <akinnane@yahoo.co.uk>
2018-10-21 19:26:00 +0100

    Add debug code

--- add_ami_to_template_parameters.py
+++ add_ami_to_template_parameters.py
@@ -6,14 +6,14 @@ manifest = {}
 with open('manifest.json') as manifest_file:
     manifest = json.load(manifest_file)
 ami_id = manifest['builds'][0]['artifact_id'].split(':')[1]
-
+print(manifest)
 
 # Update the cf-parameters.json
 with open('cf-parameters.json', 'r+') as parameters_file:
     parameters = json.load(parameters_file)
 
     parameters['Parameters']['AMIId'] = ami_id
-
+    print(parameters)
     parameters_file.seek(0)
     json.dump(parameters, parameters_file)
     parameters_file.truncate()
